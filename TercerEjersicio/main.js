1bf3e423bea31cc7a736b9f493a1f3c6e9708ba0
Zn-TaG <cabezasibarbe@gmail.com>
2025-07-13 12:22:51 -0400

    Push del tercer ejercicio

---
+++ TercerEjersicio/main.js
@@ -0,0 +1,16 @@
+class animal {
+    constructor(name, age, species) {
+        this.name = name;
+        this.age = age;
+        this.species = species;
+    }
+    verInfo(){
+            document.writeln("hola");
+        }
+} 
+
+
+const dog = new animal("Rex", 5, "Dog");
+
+document.writeln("hola")
+
